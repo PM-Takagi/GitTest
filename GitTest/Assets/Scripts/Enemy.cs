54c793c1e8e040d447c15e912aff5e17ac09a716
PM-Uda <uda@pmc.co.jp>
2020-03-03 15:15:11 +0900

    Enemyを実装しました。

---
+++ GitTest/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    float m_befPosY = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_befPosY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sin = Mathf.Sin(Time.time);
+        float cos = Mathf.Cos(Time.time);
+        transform.position = new Vector3(cos, sin + m_befPosY, 0);
+    }
+}
