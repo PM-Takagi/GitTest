700e01449eaf53585947a4abe529d1c8aecb7cf8
PM-Uda <uda@pmc.co.jp>
2020-03-03 15:45:09 +0900

    Revert "Enemyに上下動を追加しました。"
    This reverts commit 2e4973fe74311bfc960b08cd86a6e9c782996cda.

--- GitTest/Assets/Scripts/Enemy.cs
+++ GitTest/Assets/Scripts/Enemy.cs
@@ -6,12 +6,10 @@ public class Enemy : MonoBehaviour
 {
 
     float m_befPosY = 0f;
-    float m_vec = 1;
     // Start is called before the first frame update
     void Start()
     {
         m_befPosY = transform.position.y;
-        m_vec = -1;
     }
 
     // Update is called once per frame
@@ -19,12 +17,6 @@ public class Enemy : MonoBehaviour
     {
         float sin = Mathf.Sin(Time.time);
         float cos = Mathf.Cos(Time.time);
-        m_befPosY += 0.01f * m_vec;
         transform.position = new Vector3(cos, sin + m_befPosY, 0);
-
-        if(transform.position.y >= 4 || transform.position.y <= -4)
-        {
-            m_vec *= -1;
-        }
     }
 }
